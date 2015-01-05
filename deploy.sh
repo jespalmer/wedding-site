6eff650f322c23c93005758977e0be57c348b22b
John Palgut <Jwsonic@gmail.com>
2015-01-04 18:25:22 -0600

    adding deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git branch -D gh-pages
+git push origin :gh-pages
+git checkout --orphan gh-pages
+git reset
+
+#grunt build
+#cp -r dist/* .
+#rm .gitignore
+
+#git add index.html 404.html bower_components/ favicon.ico images/ photos.json scripts/ styles/ views/
+#git commit -m "deploying"
