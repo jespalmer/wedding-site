d56febc18b20a734ca9cb0538e41abc9ea60624c
John Palgut <Jwsonic@gmail.com>
2015-01-05 14:45:00 -0600

    change how deploy works

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,9 @@ git reset
 
 ./make_photos.py dist/images dist/photos.json
 cp -r dist/* .
-rm .gitignore
 
 git add index.html 404.html bower_components/ favicon.ico images/ photos.json scripts/ styles/ views/
 git commit -m "deploying pages"
 git push origin gh-pages
-git clean -fd
+git clean -fxd -e node_modules -e bower_components
 git checkout master
