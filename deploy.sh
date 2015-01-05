d707dc88ca6d2d1a3007f9ae7e4da892402ab48e
John Palgut <Jwsonic@gmail.com>
2015-01-04 18:29:34 -0600

    change up deploy and favicon

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,12 @@ git push origin :gh-pages
 git checkout --orphan gh-pages
 git reset
 
-#grunt build
-#cp -r dist/* .
-#rm .gitignore
+grunt build
+cp -r dist/* .
+rm .gitignore
 
-#git add index.html 404.html bower_components/ favicon.ico images/ photos.json scripts/ styles/ views/
-#git commit -m "deploying"
+git add index.html 404.html bower_components/ favicon.ico images/ photos.json scripts/ styles/ views/
+git commit -m "deploying pages"
+git push origin gh-pages
+git clean -fd
+git checkout master
