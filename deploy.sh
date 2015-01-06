75c80a35af5b1cb9daecc0615da061fa4891699d
John Palgut <Jwsonic@gmail.com>
2015-01-05 18:17:46 -0600

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git reset
 ./make_photos.py dist/images dist/photos.json
 cp -r dist/* .
 
-git add -f index.html 404.html bower_components/ favicon.ico images/ photos.json scripts/ styles/ views/
+git add -f index.html 404.html bower_components/ favicon.ico images/ photos.json scripts/ styles/ views/ CNAME
 git commit -m "deploying pages"
 git push origin gh-pages
 git clean -fxd -e node_modules -e bower_components
